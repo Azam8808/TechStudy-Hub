fde4cf0cece15bcb1ae47011d6679b76fe8ec71f
azam-clb <mohd.azam.fsd@colibyt.com>
2025-01-31 18:17:59 +0530

    update code

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000",
+		"https://tech-study-hub.vercel.app",
+		],
 		credentials:true,
 	})
 )
