cc76264bdb8066422df9ce8d66f84892d1f18721
azam-clb <mohd.azam.fsd@colibyt.com>
2025-02-05 10:01:54 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -21,10 +21,13 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "*",  // This is for testing purposes, won't work with credentials
-    credentials: true,
-}));
+
+app.use(
+    cors({
+        origin:"*",
+        credentials:true,
+    })
+);
 
 app.use(
 	fileUpload({
